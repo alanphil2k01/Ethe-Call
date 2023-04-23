b50b7386be00a7e248e41c23d738ddcb6507b6b7
Alan Philipose <alanphilipose@gmail.com>
2023-04-23 11:27:55 +0530

    Added profile page

---
+++ web/app/profile/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Fingerprint } from "@/app/fingerprint";
+import { useContext, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+import { Blockchain } from "../blockchain";
+
+export default function Profile() {
+    const nicknameRef = useRef<HTMLInputElement>();
+
+    const router = useRouter();
+
+    const { loadedWeb3, signer, setFingerprint, setNickname } = useContext(Blockchain);
+    const { generateNewCertificate } = useContext(Fingerprint);
+
+    useEffect(() => {
+        console.log(signer);
+        console.log(loadedWeb3);
+        if (!loadedWeb3) {
+            alert("Not connected to metamask");
+            router.push("/");
+        }
+    }, []);
+
+    async function generate() {
+        const cert = await generateNewCertificate();
+        const fingerprint = (cert.getFingerprints())[0].value;
+        console.log(fingerprint);
+        console.log(fingerprint.length);
+        await setFingerprint(fingerprint);
+    }
+
+    async function set() {
+        const nickname = nicknameRef.current.value;
+        await setNickname(nickname);
+        nicknameRef.current.value = "";
+    }
+
+    return (
+        <>
+            <button onClick={generate}>Generate Certificate</button>
+            <button>Set Certificate</button>
+            <button>Get Certificate</button>
+            <input ref={nicknameRef} className="border-2 border-black" type="text" />
+            <button onClick={set}>Set Nickname</button>
+        </>
+    )
+}
