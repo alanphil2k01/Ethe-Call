7fd28eed0f05d7a9d644f21cfbc0f7cd13c88f45
Vishnu <21vishnuvinod@gmail.com>
2023-04-23 22:56:35 +0530

    styled chat component

--- web/components/ChatComponent.tsx
+++ web/components/ChatComponent.tsx
@@ -1,29 +1,61 @@
-
-import React from "react";
-
-
+import React, { useState } from "react";
+import styles from "./ChatComponent.module.css"; // Import the CSS module
 
 const ChatComponent = ({ msgs, chatInputRef, sendChatMsg }) => {
+  const [isOpen, setIsOpen] = useState(false); // State to track if chat is open or closed
+
+  const toggleChat = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <ul>
-        {msgs.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        ref={chatInputRef}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            sendChatMsg();
-          }
-        }}
-      />
-      <button onClick={sendChatMsg}>Send</button>
+      {!isOpen && ( // Show "O" button when chat is closed
+        <button onClick={toggleChat} className={styles.openButton}>
+         Open Chat
+        </button>
+      )}
+      {isOpen && ( // Show chat window when chat is open
+        <div className={styles.container}>
+          <div className={styles.titleBar}>
+            <h3 className={styles.title}>Chat</h3> {/* Added "Chat" heading */}
+            <button
+              onClick={toggleChat}
+              className={`${styles.closeButton} ${styles.topRight}`}
+            >
+              X
+            </button>
+          </div>
+          <div className={styles.chatContainer}>
+            {/* Chat messages */}
+            <ul className={styles.list}>
+              {msgs.map((msg, index) => (
+                <li key={index} className={styles.li}>
+                  {msg}
+                </li>
+              ))}
+            </ul>
+            {/* Chat input and Send button */}
+            <div className={styles.inputContainer}>
+              <input
+                type="text" placeholder="Enter chat"
+                ref={chatInputRef}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    sendChatMsg();
+                  }
+                }}
+                className={styles.input}
+              />
+              <button onClick={sendChatMsg} className={styles.button}>
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-
 
 export default ChatComponent;
